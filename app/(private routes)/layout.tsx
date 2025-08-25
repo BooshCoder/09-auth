3727e62f385829c3cbcc8b3711def519efde1074
Eugene <mypro4s@icloud.com>
2025-08-25 18:45:01 +0300

    fix: remove duplicate AuthProvider from private routes layout

--- app/(private routes)/layout.tsx
+++ app/(private routes)/layout.tsx
@@ -1,14 +1,9 @@
 import { ReactNode } from 'react';
-import AuthProvider from '../../components/AuthProvider/AuthProvider';
 
 export default function PrivateLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  return (
-    <AuthProvider>
-      {children}
-    </AuthProvider>
-  );
+  return <>{children}</>;
 }
