73626fd89674f0ffbb3c153beec596939c4f24d5
Eugene <mypro4s@icloud.com>
2025-08-25 15:12:10 +0300

    fix serverApi.ts: import User type from types/user.ts instead of clientApi

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import type { User, LoginParams, RegisterParams } from './clientApi';
+import type { User } from '../../types/user';
+import type { LoginParams, RegisterParams } from './clientApi';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
